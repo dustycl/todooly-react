2ba5fc4c1e99e54e86101cddd33a04fd116a1335
Dustin Lamperts <dustycl@gmail.com>
2021-05-09 22:07:59 -0700

    Add Activity Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react'
 
+const Activity = (props) => {
+  <li>
+    {props.activityName}
+    <button
+     className="muted-button"
+    >Delete</button>
+  </li>
+}
+
 class App extends Component {
   render() {
     return (
-      <div class="container">
+      <div class="small-container">
         <form className="flex">
           <input
            type="text"
            ></input>
           <button className="full-button">Add activity</button>
         </form>
+        <ul>
+          <li>Activity name <button className="muted-button">Delete</button></li>
+        </ul>
       </div>
     )
   }
