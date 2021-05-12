0d1449c69180c968122f9fc291479862820061a1
Dustin Lamperts <dustycl@gmail.com>
2021-05-12 16:33:58 -0700

    Refactor Activity to use only css to control hover state

--- src/List.js
+++ src/List.js
@@ -1,37 +1,17 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 
 
-class Activity extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            class: "activity",
-        }
-    }
-
-    handleMouseEnter = () => {
-        this.setState({class: this.state.class.concat(" activity-hover")});
-    }
-
-    handleMouseLeave = () => {
-        this.setState({class: "activity"})
-    }
-
-    render() {
-        return (
-            <div
-             className={this.state.class}
-             onMouseEnter={this.handleMouseEnter}
-             onMouseLeave={this.handleMouseLeave} >
-                <input type="checkbox" />
-                <div>{this.props.item}</div>
-                <div>
-                    <button className="muted-button activity-button-delete">Delete</button>
-                </div>
+const Activity =(props) => {
+    return (
+        <div className="activity">
+            <input type="checkbox" />
+            <div>{props.item}</div>
+            <div>
+                <button className="muted-button activity-button-delete">Delete</button>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const Activities = (props) => {
