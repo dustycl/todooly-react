f50e69f1366ca1891ecdd191b8af3a9b8fb4d5a6
Dustin Lamperts <dustycl@gmail.com>
2021-07-07 15:36:46 -0700

    Add sidenav for tags

---
+++ src/Sidenav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidenav = () => {
+    return (
+        <div>
+            <h2>Dustin</h2>
+            <h3>Tags</h3>
+            <ul>
+                <li>Home</li>
+                <li>Chores</li>
+                <li>School</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidenav
